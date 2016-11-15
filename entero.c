4da8add61504b2b5e837f921063ffcb9b4e06a05
dacasas <dacasas@uc.cl>
2016-11-15 18:42:18 -0300

    blbla

--- entero.c
+++ entero.c
@@ -6,13 +6,13 @@ Integer *new_integer(char **number_string) {
   return i;
 }
 
-Integer *add(Integer *i1, Integer *i2) {
+Integer *add(Integer *a, Integer *b) {
   Integer *i = (Integer *)malloc(sizeof(Integer));
   // TODO Implements addition methos for Integer struct
   return i;
 }
 
-Integer *mupltiply(Integer *i1, Integer *i2) {
+Integer *mupltiply(Integer *a, Integer *b) {
   Integer *i = (Integer *)malloc(sizeof(Integer));
   // TODO Implements multiplication for Integer struct
   return i;
@@ -28,4 +28,38 @@ Integer *division_whole(Integer *numerator, Integer *divider, Integer **rest) {
   Integer *i = (Integer *)malloc(sizeof(Integer));
   // TODO Implements division for Integer struct
   return i;
+}
+
+Integer *power(Integer *base, Integer *exponent) {
+  Integer *i = (Integer *)malloc(sizeof(Integer));
+  // TODO Implements powers
+  return i;
+}
+
+char equals(Integer *a, Integer *b) {
+  if (a->digits_count != b->digits_count) {
+    return 0;
+  } else {
+    for (size_t i = 0; i < a->digits_count; i++) {
+      if (a->digits[i] != b->digits[i]) {
+        return 0;
+      }
+    }
+    return 1;
+  }
+}
+
+Integer *gcd(Integer *a, Integer *b) {
+  Integer *zero = new_integer(0);
+  if (a == 0 && b == 0) {
+    return 0;
+  } else if (a == 0) {
+    return b;
+  } else if (b == 0) {
+    return a;
+  } else if (a >= b) {
+    return gcd(b, module(a, b));
+  } else {
+    return gcd(a, module(b, a));
+  }
 }
--- entero.h
+++ entero.h
@@ -14,10 +14,10 @@ typedef struct integer Integer;
 Integer *new_integer(char **number_string);
 
 /* Entrega la suma de ambos enteros */
-Integer *add(Integer *i1, Integer *i2);
+Integer *add(Integer *a, Integer *b);
 
 /* Entrega la multiplicacion de ambos enteros */
-Integer *mupltiply(Integer *i1, Integer *i2);
+Integer *mupltiply(Integer *a, Integer *b);
 
 /* Entrega (numero modulo base) */
 Integer *module(Integer *number, Integer *base);
@@ -25,3 +25,9 @@ Integer *module(Integer *number, Integer *base);
 /* Entrega la division entera entre numerador y divisor, y deja el resto en rest
  */
 Integer *division_whole(Integer *numerator, Integer *divider, Integer **rest);
+
+/* Entrega el resultado de base^exponent */
+Integer *power(Integer *base, Integer *exponent);
+
+/* Entrega el maximo comun divisor (MCD) entre a y b */
+Integer *gcd(Integer *a, Integer *b);
